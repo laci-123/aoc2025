12f083692a2c9aa4c4872483f40634c7e20e0773
Gohér László <goher.laci@gmail.com>
2025-12-02 12:45:40 +0100

    day 2 part 1

---
+++ day02/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Range struct {
+	from int
+	to int
+}
+
+func readInput() ([]Range, error) {
+	line, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		return nil, err
+	}
+	ranges := make([]Range, 0)
+	for r := range strings.SplitSeq(string(line), ",") {
+		from_to := strings.Split(r, "-")
+		from, err := strconv.Atoi(strings.Trim(from_to[0], " \n"))
+		if err != nil {
+			return nil, err
+		}
+		to, err := strconv.Atoi(strings.Trim(from_to[1], " \n"))
+		if err != nil {
+			return nil, err
+		}
+		ranges = append(ranges, Range{from: from, to: to})
+	}
+	return ranges, nil
+}
+
+func isRepeated(s string) bool {
+	m := len(s)/2
+	return s[:m] == s[m:]
+}
+
+
+func part1(ranges []Range) int {
+	sum := 0
+	for _, r := range ranges {
+		for i := r.from; i <= r.to; i += 1 {
+			if isRepeated(strconv.Itoa(i)) {
+				sum += i
+			}
+		}
+	}
+	return sum
+}
+
+func main() {
+	ranges, err := readInput()
+	if err != nil {
+		panic(err)
+	} 
+
+	result1 := part1(ranges)
+	fmt.Println("part 1 result: ", result1)
+}
