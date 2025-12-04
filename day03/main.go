1029a9ef55e3a58e8f8e71e6ea50f7055f664cc1
Gohér László <goher.laci@gmail.com>
2025-12-04 13:53:30 +0100

    day 3 part 2

--- day03/main.go
+++ day03/main.go
@@ -33,6 +33,63 @@ func readInput() ([]BatteryBank, error) {
 	return bank, nil
 }
 
+func pow10(p int) int {
+	x := 1
+	for i := 0; i < p ; i += 1 {
+		x *= 10
+	}
+	return x
+}
+
+func calcJoltage(digits map[int]int, newDigit int, newDigitPos int, k int) int {
+	p := 1
+	sum := 0
+	for i := k-1; i >= 0; i -= 1 {
+		if i == newDigitPos {
+			sum += newDigit * p
+			p *= 10
+		} else if d, ok := digits[i]; ok {
+			sum += d * p
+			p *= 10
+		}
+	}
+	// fmt.Printf(">>> %v, %v, %v --> %v\n", digits, newDigitPos, newDigit, sum)
+	return sum
+}
+
+func findMaxJoltage(bank BatteryBank, n int) (digits map[int]int, max int) {
+	if n == 1 {
+		digits := make(map[int]int)
+		max := 0
+		maxPos := 0
+		for i := 0; i < len(bank.batteries); i += 1 {
+			if bank.batteries[i] > max {
+				max = bank.batteries[i]
+				maxPos = i
+			}
+		}
+		digits[maxPos] = max
+		return digits, max
+	} else {
+		digits, max := findMaxJoltage(bank, n - 1)
+		maxDigit := 0
+		maxDigitPos := 0
+		for i := 0; i < len(bank.batteries); i += 1 {
+			if _, ok := digits[i]; ok {
+				continue
+			}
+			x := calcJoltage(digits, bank.batteries[i], i, len(bank.batteries))
+			if x > max {
+				max = x
+				maxDigitPos = i
+				maxDigit = bank.batteries[i]
+			}
+		}
+		digits[maxDigitPos] = maxDigit
+		return digits, max
+	}
+}
+
 func part1(banks []BatteryBank) int {
 	sum := 0
 	for _, bank := range banks {
@@ -50,6 +107,14 @@ func part1(banks []BatteryBank) int {
 	return sum
 }
 
+func part2(banks []BatteryBank) int {
+	sum := 0
+	for _, bank := range banks {
+		_, x := findMaxJoltage(bank, 12)
+		sum += x
+	}
+	return sum
+}
 
 func main() {
 	banks, err := readInput()
@@ -60,6 +125,6 @@ func main() {
 	result1 := part1(banks)
 	fmt.Println("part 1 result: ", result1)
 
-	// result2 := part2(ranges)
-	// fmt.Println("part 2 result: ", result2)
+	result2 := part2(banks)
+	fmt.Println("part 2 result: ", result2)
 }
