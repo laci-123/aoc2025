892c48e92b29d417e1a4df18f9745356d7fad713
Gohér László <goher.laci@gmail.com>
2025-12-08 18:36:13 +0100

    day 6 part 2

--- day06/main.go
+++ day06/main.go
@@ -36,6 +36,22 @@ func readInput() ([][]string, error) {
 	return words, nil
 }
 
+func readInput2() ([][]rune, error) {
+	content, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		return nil, err
+	}
+	runes := make([][]rune, 0)
+	for line := range strings.Lines(string(content)) {
+		cLine := make([]rune, 0)
+		for _, c := range line {
+			cLine = append(cLine, c)
+		}
+		runes = append(runes, cLine)
+	}
+	return runes, nil
+}
+
 func part1(words [][]string) (int, error) {
 	sum := 0
 	for i := 0; i < len(words[0]); i += 1 {
@@ -65,15 +81,76 @@ func part1(words [][]string) (int, error) {
 	return sum, nil
 }
 
-func main() {
-	words, err := readInput()
-	if err != nil {
-		panic(err)
-	} 
+func part2(runes [][]rune) (int, error) {
+	sum := 0
+	columns := make([]int, 0)
+	var op rune
+	for x := len(runes[0])-1; x >= 0; x -= 1 {
+		n := 0
+		p := 1
+		allSpace := true
+		for y := len(runes)-1; y >= 0; y -= 1 {
+			c := runes[y][x]
+			switch c {
+			case '+':
+				allSpace = false
+				op = '+'
+			case '*':
+				allSpace = false
+				op = '*'
+			case ' ', '\n':
+				continue
+			default:
+				allSpace = false
+				n += int(c - '0') * p
+				p *= 10
+			}
+		}
+		if !allSpace || x == 0 {
+			columns = append(columns, n)
+		}
+		if allSpace || x == 0 {
+			var x int
+			switch op {
+			case '+':
+				x = 0
+			case '*':
+				x = 1
+			}
+			for _, n := range columns {
+				switch op {
+				case '+':
+					x += n
+				case '*':
+					x *= n
+				}
+			}
+			sum += x
+			columns = columns[:0]
+		}
+	}
+	return sum, nil
+}
 
-	result1, err := part1(words)
+func main() {
+	// words, err := readInput()
+	// if err != nil {
+	// 	panic(err)
+	// } 
+	// result1, err := part1(words)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("part 1 result: ", result1)
+
+	runes, err := readInput2()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("part 1 result: ", result1)
+
+	result2, err := part2(runes)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("part 2 result: ", result2)
 }
