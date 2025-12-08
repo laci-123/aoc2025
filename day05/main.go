051a39de13e79ccc91433af2507d6b78ab73d11b
Gohér László <goher.laci@gmail.com>
2025-12-08 16:42:11 +0100

    day5 part 2

--- day05/main.go
+++ day05/main.go
@@ -63,6 +63,70 @@ func part1(db Database) int {
 	return sum
 }
 
+func rangeConcat(rng1, rng2 IdRange) (bool, IdRange) {
+	if rng1.end < rng2.begin || rng2.end < rng1.begin {
+		return false, IdRange{-1, -1}
+	}
+	if rng1.begin == rng2.begin && rng1.end == rng2.end {
+		return false, IdRange{-1, -1}
+	}
+	return true, IdRange{begin: min(rng1.begin, rng2.begin), end: max(rng1.end, rng2.end)}
+}
+
+func rangeConcatMany(rngs map[IdRange]bool) map[IdRange]bool {
+	newRngs := make(map[IdRange]bool)
+	concatHappend := false
+	for rng1, b1 := range rngs {
+		if !b1 {
+			continue
+		}
+		for rng2, b2 := range rngs {
+			if !b2 {
+				continue
+			}
+			ok, newRng := rangeConcat(rng1, rng2)
+			if ok {
+				newRngs[newRng] = true
+				concatHappend = true
+				if !(rng1.begin == newRng.begin && rng1.end == newRng.end) {
+					newRngs[rng1] = false
+				}
+				if !(rng2.begin == newRng.begin && rng2.end == newRng.end) {
+					newRngs[rng2] = false
+				}
+			} else {
+				if _, ok := newRngs[rng1]; !ok {
+					newRngs[rng1] = true
+				}
+				if _, ok := newRngs[rng2]; !ok {
+					newRngs[rng2] = true
+				}
+			}
+		}
+	}
+	if concatHappend {
+		return rangeConcatMany(newRngs)
+	} else {
+		return newRngs
+	}
+}
+
+func part2(db Database) int {
+	rngs := make(map[IdRange]bool)
+	for _, rng := range db.ranges {
+		rngs[rng] = true
+	}
+	newRngs := rangeConcatMany(rngs)
+	sum := 0
+	for rng, b := range newRngs {
+		if !b {
+			continue
+		}
+		sum += rng.end - rng.begin + 1
+	}
+	return sum
+}
+
 
 func main() {
 	db, err := readInput()
@@ -72,4 +136,6 @@ func main() {
 
 	result1 := part1(db)
 	fmt.Println("part 1 result: ", result1)
+	result2 := part2(db)
+	fmt.Println("part 2 result: ", result2)
 }
